de78b3f17a26daf1bd110c78012a90605a6dae12
Tito Ribeiro Motter <titoribeiromotter@gmail.com>
2025-08-09 03:19:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,13 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('min-h-screen bg-background font-body antialiased', ptSans.variable)}>
+      <body className={cn('min-h-screen bg-background font-body antialiased', ptSans.variable)} suppressHydrationWarning>
         <AuthProvider>
             {children}
             <Toaster />
