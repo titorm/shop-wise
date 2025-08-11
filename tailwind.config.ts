edf8c994791583cc5878be3f7e5daebb926abf4a
Tito Ribeiro Motter <titoribeiromotter@gmail.com>
2025-08-11 19:58:58 +0000

    delete colors "chart-1", "chart-2"...

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,13 +55,6 @@ export default {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
-        chart: {
-          '1': 'hsl(var(--chart-1))',
-          '2': 'hsl(var(--chart-2))',
-          '3': 'hsl(var(--chart-3))',
-          '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))',
-        },
         category: {
           hortifruti: 'hsl(var(--category-hortifruti))',
           'hortifruti-foreground': 'hsl(var(--category-hortifruti-foreground))',
