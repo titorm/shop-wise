1cc32183789e7ba73d4edaa4704c3ef394515dde
Tito Ribeiro Motter <titoribeiromotter@gmail.com>
2025-08-12 14:13:10 +0000

    o hover de alguns botões está em azul. Alterar para a cor primária se fo

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -524,9 +524,9 @@ const sidebarMenuButtonVariants = cva(
   {
     variants: {
       variant: {
-        default: "hover:bg-sidebar-accent hover:text-sidebar-accent-foreground",
+        default: "",
         outline:
-          "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]",
+          "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]",
       },
       size: {
         default: "h-10 text-sm",
