2e77ceaa5c5bb7d70e3dcbe2bf686e563d0ad10a
Tito Ribeiro Motter <titoribeiromotter@gmail.com>
2025-08-12 10:43:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head />
       <body
         className={cn('min-h-screen bg-background font-body antialiased', noto_sans.variable)}
+        suppressHydrationWarning
       >
         <AuthProvider>
             {children}
