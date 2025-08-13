fd59d86f0548cf557e799f612f98b9c9b29c59e2
Tito Ribeiro Motter <titoribeiromotter@gmail.com>
2025-08-13 12:28:06 +0000

    a cor não está pegando no texto da logo, mas não precisa, pode utilizar

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,4 @@
+
 import Link from "next/link";
 import { LoginForm } from "@/components/auth/login-form";
 import { ShopWiseIcon } from "@/components/icons";
@@ -7,7 +8,7 @@ export default function LoginPage() {
     <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
       <div className="w-full max-w-md">
         <Link href="/" className="flex items-center justify-center mb-8">
-            <ShopWiseIcon className="w-10 h-10 text-primary" />
+            <ShopWiseIcon className="w-16 h-16 text-primary" />
         </Link>
         <LoginForm />
       </div>
