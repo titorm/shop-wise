4eab3eefa94615d09b76e85c6cd8762f061170af
Tito Ribeiro Motter <titoribeiromotter@gmail.com>
2025-08-14 22:48:19 +0000

    ajuste o i18n e publique as mudanças no git (git push)

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -6,7 +6,6 @@ import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-// Do not init i18n here. It will be initialized in providers.tsx
 i18n
   .use(HttpBackend)
   .use(LanguageDetector)
@@ -26,7 +25,6 @@ i18n
     react: {
       useSuspense: false, // This is important to not use suspense
     },
-    initImmediate: false,
   });
 
 export default i18n;
